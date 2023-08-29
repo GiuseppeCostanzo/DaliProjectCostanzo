22b669515b637d11dc3e5e91a0ef797f4c2ae1f7
GiuseppeCostanzo <64311248+GiuseppeCostanzo@users.noreply.github.com>
2023-08-29 11:56:47 +0200

    Versione con 5 agenti totali

--- lavoro/Progetto/startmas.bat
+++ lavoro/Progetto/startmas.bat
@@ -7,7 +7,7 @@ set main_home=..\..
 set dali_home=..\..\src
 set conf_dir=conf
 set prolog=%sicstus_home%\spwin.exe
-set WAIT=ping -n 4 127.0.0.1
+set WAIT=ping -n 8 127.0.0.1
 
 ::del /q tmp\*.*
 del /q work\*.pl
